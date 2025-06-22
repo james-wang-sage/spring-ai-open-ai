203f509a83ebcb779f65399a3a1e20c8b977e750
James Wang <james.wang@sage.com>
2025-06-22 16:44:18 -0700

    Update Lombok version to 1.18.36 and refactor Logger usage

--- src/main/java/uladzislau/zhurauliou/spring_ai_open_ai/service/LoggerAdvisor.java
+++ src/main/java/uladzislau/zhurauliou/spring_ai_open_ai/service/LoggerAdvisor.java
@@ -1,15 +1,17 @@
 package uladzislau.zhurauliou.spring_ai_open_ai.service;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.AdvisedRequest;
 import org.springframework.ai.chat.client.RequestResponseAdvisor;
 import org.springframework.ai.chat.metadata.ChatResponseMetadata;
 import org.springframework.ai.chat.model.ChatResponse;
 
-import java.util.Map;
-
-@Slf4j
 public class LoggerAdvisor implements RequestResponseAdvisor {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggerAdvisor.class);
 
     @Override
     public AdvisedRequest adviseRequest(AdvisedRequest request, Map<String, Object> context) {
